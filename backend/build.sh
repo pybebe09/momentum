fbf15e7c21b0ba9b14d530e1eab715fc7d8d753a
begi7288 <begi7288@gmail.com>
2026-07-22 15:05:16 +0500

    Add Render deployment dependencies to requirements.txt

--- backend/build.sh
+++ backend/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# exit on error
-set -o errexit
+set -e
 
+pip install --upgrade pip
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py migrate --noinput
 python manage.py seed_data
